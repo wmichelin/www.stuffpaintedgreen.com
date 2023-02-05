f8e761cdaf9aa936b03ed8f48e983f5b0d7fc9b1
Walter Michelin <wmichelin@gmail.com>
2023-02-05 09:54:44 -0500

    Add google analytics tag

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LR79RFF6EM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LR79RFF6EM');
+      `}
+      </Script>
+
       <Head>
         <title>Stuff Painted Green</title>
         <link rel="icon" href="/favicon.ico" />
