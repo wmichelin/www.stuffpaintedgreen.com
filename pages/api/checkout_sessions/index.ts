ae52791cbf6e773b109f6cf84acbdf3ae966c6db
Walter Michelin <wmichelin@gmail.com>
2023-02-05 20:27:31 -0500

    Add dynamic payment metadata info

--- pages/api/checkout_sessions/index.ts
+++ pages/api/checkout_sessions/index.ts
@@ -1,22 +1,41 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: "2022-11-15",
+})
+
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params: Stripe.Checkout.SessionCreateParams = {
         line_items: [
           {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: 'price_1MYItvLni27QE7t34Ay4ULuo',
             quantity: 1,
+            price_data: {
+              product_data: {
+                name: "the product",
+                metadata: {
+                  "foo": "test this: " + Math.random(),
+                },
+              },
+              currency: "USD",
+              tax_behavior: "inclusive",
+              unit_amount: 101,
+            },
           },
         ],
         mode: 'payment',
+        metadata: {
+          "foo": "test this: " + Math.random(),
+        },
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
         automatic_tax: {enabled: true},
-      });
+      };
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
